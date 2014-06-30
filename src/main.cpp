d46dd3183d2b21946ee59b12ce374a4df6aae5a6
Finalhash <pool.finalhash.com@db03.pcmerc.net>
2014-06-30 10:42:02 -0600

    Retarget every 30 seconds and New Block every 10 seconds

--- src/main.cpp
+++ src/main.cpp
@@ -1081,8 +1081,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 3.5 * 24 * 60 * 60; // UnitedScryptCoin: 3.5 days
-static const int64 nTargetSpacing = 2.5 * 60; // UnitedScryptCoin: 2.5 minutes
+static const int64 nTargetTimespan = 30; // UnitedScryptCoin: 3.5 days
+static const int64 nTargetSpacing = 10; // UnitedScryptCoin: 2.5 minutes
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 //
